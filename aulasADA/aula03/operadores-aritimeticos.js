92db39ce975c85bb891aa8d3566ca7bd0879c94b
Rafael Torales de Souza <rafaeltoraless@hotmail.com>
2023-05-29 17:00:20 -0300

    Operadores Aritméticos

---
+++ aulasADA/aula03/operadores-aritimeticos.js
@@ -0,0 +1,30 @@
+// Operadores Aritméticos
+
+let numero1 = 20;
+let numero2 = 2;
+
+// Adição:            +
+// Subtração:         -
+// Multiplicação:     *
+// Divisão:           /
+// Exponenciação:     **
+// Resto da divisão:  %
+
+const soma = numero1 + numero2;
+
+console.log("Soma =", soma);
+console.log("Subtração =", numero1 - numero2);
+console.log("Multiplicação = ", numero1 * numero2);
+console.log("Divisão = ", numero1 / numero2);
+console.log("Exponenciação = ", numero1 ** numero2);
+console.log("Resto da divisão = ", numero1 % 3);
+
+// Precedência de operadores
+
+console.log((3 + 7) * 3);
+console.log((10 + 7 + 8) / 3);
+
+// Biblioteca Math
+
+console.log(Math.PI);
+console.log(Math.sqrt(16));
