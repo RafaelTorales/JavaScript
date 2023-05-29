ca46bf5e628e73b215cd6d2c112df6ef4b14e9a2
Rafael Torales de Souza <rafaeltoraless@hotmail.com>
2023-05-29 16:42:21 -0300

    Segunda aula

---
+++ aulasADA/aula02/variaveis.js
@@ -0,0 +1,30 @@
+// Variavéis
+
+let idade = 26; // Criando a primeira variável
+
+console.log(idade);
+
+// Tipos de variavéis
+
+idade = 30; // Number
+let numero = 30; // Number
+let altura = 1.77; // Number
+let nome = "Rafael"; // String (texto)
+let estudando = true; // Booleano (true ou false)
+
+console.log(numero, typeof numero);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando);
+
+let outra = undefined;
+
+console.log(outra);
+
+let nula = null;
+
+console.log(nula);
+
+var variavel = 30; // Não utilize o var
+
+const constante = 100;
